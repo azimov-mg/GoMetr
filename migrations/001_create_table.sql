5de224a49d5b7bf9d2276ee2468fd3222abfe1c9
Murodjon Azimov <murodjon.azimov.99@mail.ru>
2023-05-28 14:56:12 +0500

    final stage

--- migrations/001_create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE metrics (
-                         id SERIAL PRIMARY KEY,
-                         timestamp TIMESTAMP DEFAULT NOW(),
-                         name TEXT NOT NULL,
-                         value DOUBLE PRECISION NOT NULL
-);
-
-CREATE INDEX idx_metrics_timestamp ON metrics (timestamp);
-CREATE INDEX idx_metrics_name ON metrics (name);
